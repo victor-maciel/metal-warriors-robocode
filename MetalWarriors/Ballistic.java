de9c9fa6d0714f39411c5019897f3d1ae9ee6b66
Victor Maciel <31293720+victor-maciel@users.noreply.github.com>
2019-05-20 16:45:48 -0300

    Add files via upload

--- MetalWarriors/Ballistic.java
+++ MetalWarriors/Ballistic.java
@@ -1,15 +1,16 @@
 package MetalWarriors;
 
 import robocode.HitRobotEvent;
-import robocode.Robot;
 import robocode.ScannedRobotEvent;
+import robocode.AdvancedRobot;
+import robocode.*;
 
 import java.awt.*;
 
 /**
  * Ballistic - a robot by (Victor Maciel)
  */
-public class Ballistic extends Robot{
+public class Ballistic extends AdvancedRobot{
 int turnDirection = 1; // Clockwise or counterclockwise
 
 	/**
@@ -17,12 +18,20 @@ int turnDirection = 1; // Clockwise or counterclockwise
 	 */
 	public void run() {
 		// Set colors
-		setBodyColor(Color.magenta);
-		setGunColor(Color.magenta);
-		setRadarColor(Color.gray);
+		
 
 		while (true) {
-			turnRight(5 * turnDirection);
+			
+			if(pertoParede()){
+					corRed();
+					back(30);
+			}else{
+			
+					corBlack();
+					setTurnLeft(10000);
+					setMaxVelocity(5);
+					ahead(10000);
+			}
 		}
 	}
 
@@ -30,25 +39,26 @@ int turnDirection = 1; // Clockwise or counterclockwise
 	 * onScannedRobot:  We have a target.  Go get it.
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-
-		if (e.getBearing() >= 0) {
-			turnDirection = -1;
-		} else {
-			turnDirection = 1;
-		}
-
-		turnRight(e.getBearing());
-		ahead(e.getDistance() + 3);
+		corRed();
+		fire(3);
+		corBlack();
 		scan(); // Might want to move ahead again!
 	}
-/*	public void onWin(WinEvent e) {	
-    		turnRight(36000);
-		}
-*/
+	
+	
+	
+	public void onHitWall(HitWallEvent e) {
+		back(20);
+	}
+
 	/**
 	 * onHitRobot:  Turn to face robot, fire hard, and ram him again!
 	 */
 	public void onHitRobot(HitRobotEvent e) {
+		ahead(100);
+		if (getGunHeat() == 0) {
+			fire(Rules.MAX_BULLET_POWER);
+		}
 		if (e.getBearing() >= 0) {
 			turnDirection = 1;
 		} else {
@@ -60,16 +70,52 @@ int turnDirection = 1; // Clockwise or counterclockwise
 		// We want to ram him instead for bonus points
 		if (e.getEnergy() > 16) {
 			fire(3);
+			corRed();
 		} else if (e.getEnergy() > 10) {
 			fire(2);
+			corBlack();
 		} else if (e.getEnergy() > 4) {
 			fire(1);
+			corRed();
 		} else if (e.getEnergy() > 2) {
 			fire(.5);
+			corBlack();
 		} else if (e.getEnergy() > .4) {
 			fire(.1);
+			corRed();
 		}
 		ahead(40); // Ram him again!
 	}
+	
+	public boolean pertoParede()
+	{
+		return
+		(getX() < 50 || getX() > getBattleFieldWidth() - 50 ||
+		 getY() < 50 || getY() > getBattleFieldHeight() - 50);
+	}
 
+	//comemorando se ganhar 
+	public void onWin(WinEvent e)
+	{
+		turnLeft(72000);
+	}
+	
+
+	//muda a cor do robo para vermelho
+	public void corRed(){
+		setBodyColor(Color.red);
+		setGunColor(Color.red);
+		setRadarColor(Color.red);
+		setBulletColor(Color.red);
+		setScanColor(Color.red);
+	}
+	
+	//muda a cor do robo para preto
+	public void corBlack(){
+		setBodyColor(Color.black);
+		setGunColor(Color.black);
+		setRadarColor(Color.black);
+		setBulletColor(Color.black);
+		setBodyColor(Color.black);
+	}
 }
